c839f6527d7ce48792c8c8b19211e2ab74a9c35b
hsing-yu, Chen <andy19933@gmail.com>
2016-11-02 00:42:10 +0800

    Added spec_helper.rb and search_spec.rb

---
+++ spec/spec_helper.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+ENV['RACK_ENV'] = 'test'
+
+require 'minitest/autorun'
+require 'minitest/rg'
+require 'rack/test'
+require 'vcr'
+require 'webmock'
+
+require_relative '../app'
+
+include Rack::Test::Methods
+
+def app
+  ShareLearningAPI
+end
+
+FIXTURES_FOLDER = 'spec/fixtures'.freeze
+CASSETTES_FOLDER = "#{FIXTURES_FOLDER}/cassettes".freeze
+SEARCH_CASSETTE = 'search'.freeze
+
+# read credentials from a Yaml file into environment variables
+if File.file?('config/credentials.yml')
+  CREDENTIALS = YAML.load(File.read('config/app.yml'))
+  ENV['YOUTUBE_API_KEY'] = CREDENTIALS[:youtube_api_key]
+end
+
+VCR.configure do |c|
+  c.cassette_library_dir = CASSETTES_FOLDER
+  c.hook_into :webmock
+
+  c.filter_sensitive_data('<API_KEY>') do
+    ENV['YOUTUBE_API_KEY']
+  end
+end
