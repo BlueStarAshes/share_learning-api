2d7c2579831aa630ad6cebec7ca738212d25acd7
hsing-yu, Chen <andy19933@gmail.com>
2016-11-03 22:45:07 +0800

    Extend our controller class of our modular Sinatra application and copy its settings to Econfig

--- app.rb
+++ app.rb
@@ -2,4 +2,8 @@ require 'Share_learning'
 
 # Share Learning API web service
 class ShareLearningAPI < Sinatra::Base
+  extend Econfig::Shortcut
+
+  Econfig.env = settings.environment.to_s
+  Econfig.root = settings.root
 end
