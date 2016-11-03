83d777be744bce4df6fbe5e6898df585a693780a
hsing-yu, Chen <andy19933@gmail.com>
2016-11-03 21:35:28 +0800

    Added a sad test for search_spec

--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -13,7 +13,7 @@ describe 'Search Routes' do
   end
 
   describe 'Retrieve resource from Coursera, Udacity, and Youtube' do
-    it 'HAPPY: should retrieve relating resource with the given keyword' do
+    it 'HAPPY: should retrieve relating resource with the HAPPY keyword' do
       get "api/v0.1/search?#{HAPPY_SEARCH_KEYWORD}"
 
       last_response.status.must_equal 200
@@ -46,7 +46,30 @@ describe 'Search Routes' do
       end
     end
 
-    it 'SAD: should not retrieve any resource with the given keyword' do
+    it 'SAD: should retrieve no any resource with the SAD keyword' do
+      get "api/v0.1/search?#{SAD_SEARCH_KEYWORD}"
+
+      last_response.status.must_equal 200
+      last_response.content_type.must_equal 'application/json'
+      resource_data = JSON.parse(last_response.body)
+
+      available_external_apis = %w(coursera udacity youtube)
+      external_api_info_keys = %w(count courses)
+
+      available_external_apis.each do |external_api|
+        resource_data.key?(external_api).must_equal true
+
+        single_resource_data = resource_data[external_api]
+        external_api_info_keys.each do |info_key|
+          single_resource_data.key?(info_key).must_equal true
+        end
+
+        courses_count = single_resource_data['count']
+        courses_count.must_equal 0
+
+        courses = single_resource_data['courses']
+        courses.count.must_equal courses_count
+      end
     end
   end
 end
