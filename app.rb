fce3ebbddb06d1631cc8d3fc037a52a543af0e01
blureze <blureze@gmail.com>
2016-11-04 20:33:55 +0800

    add app.rb

--- app.rb
+++ app.rb
@@ -1,9 +1,19 @@
+# frozen_string_literal: true
+require 'sinatra'
+require 'econfig'
 require 'Share_learning'
 
-# Share Learning API web service
+Share Learning API web service
 class ShareLearningAPI < Sinatra::Base
   extend Econfig::Shortcut
 
   Econfig.env = settings.environment.to_s
   Econfig.root = settings.root
+
+  API_VER = 'api/v0.1'
+
+  get '/?' do
+    # "ShareLearningAPI latest version endpoints are at: /#{API_VER}/"
+    "Hello World"
+  end
 end
