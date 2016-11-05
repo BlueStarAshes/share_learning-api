7bdda5e097938f2b4cf8706f21dccf626ac2e1df
blureze <blureze@gmail.com>
2016-11-05 17:22:05 +0800

    update api_spec

--- app.rb
+++ app.rb
@@ -10,10 +10,9 @@ class ShareLearningAPI < Sinatra::Base
   Econfig.env = settings.environment.to_s
   Econfig.root = settings.root
 
-  API_VER = 'api/v0.1'
+  API_VER = 'api/v0.2'
 
   get '/?' do
-    # "ShareLearningAPI latest version endpoints are at: /#{API_VER}/"
-    "Hello World"
+    "ShareLearningAPI latest version endpoints are at: /#{API_VER}/"
   end
 end
