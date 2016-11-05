fae3d9ff772e64b9acea65b93e193f5e321ce803
hsing-yu, Chen <andy19933@gmail.com>
2016-11-05 14:45:33 +0800

    Added root route meessage to our app.rb

--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
-require 'Share_learning'
+require 'sinatra'
+require 'econfig'
+require 'share_learning'
 
 # Share Learning API web service
 class ShareLearningAPI < Sinatra::Base
@@ -6,4 +8,15 @@ class ShareLearningAPI < Sinatra::Base
 
   Econfig.env = settings.environment.to_s
   Econfig.root = settings.root
+
+  # Setting Youtube API key
+  YouTube::YouTubeAPI
+    .config
+    .update(api_key: config.YOUTUBE_API_KEY)
+
+  API_VER = 'api/v0.1'.freeze
+
+  get '/?' do
+    "ShareLearningAPI latest version endpoints are at: /#{API_VER}/"
+  end
 end
