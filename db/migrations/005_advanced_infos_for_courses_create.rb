10d3a9bcab7347cb86c95dca863f7512ce326edb
ashley <ashley830204@gmail.com>
2016-11-10 11:32:27 +0800

    update migrations 005

--- db/migrations/005_advanced_infos_for_courses_create.rb
+++ db/migrations/005_advanced_infos_for_courses_create.rb
@@ -4,9 +4,10 @@ require 'sequel'
 Sequel.migration do
   change do
     create_table(:advanced_infos_for_courses) do
-      primary_key :course_id, :advanced_info_id
-      foreign_key :course_id, :advanced_info_id
+      foreign_key :course_id
+      foreign_key :advanced_info_id
       Date :time
+      primary_key [:course_id, :advanced_info_id], :name=> :items_pk
     end
   end
 end
