19fad541545add8b0ec63d6878f9b29735420bda
ashley <ashley830204@gmail.com>
2016-11-18 18:24:30 +0800

    refactoring object parsing and Jsonification on advanced_info

--- controllers/advanced_info.rb
+++ controllers/advanced_info.rb
@@ -7,11 +7,13 @@ class ShareLearningAPI < Sinatra::Base
   get "/#{API_VER}/course/advanced_info/:id/?" do
     course_id = params[:id]
     begin
-      course = Course.find(id: course_id)
+      course_info = CourseAdvancedInfo.find(course_id: course_id)
+      advanced_info = AdvancedInfo.find(id: course_info.advanced_info_id)
 
       content_type 'application/json'
-      { title: course.title, introduction: course.introduction, \
-        source: course.source, link: course.link}.to_json
+      AdvancedInfoRepresenter.new(advanced_info).to_json
+      # { prerequisite: advanced_info.prerequisite, \
+      #   difficulty: advanced_info.difficulty, helpful: advanced_info.helpful }.to_json
     rescue 
       halt 404, 'Courses not found'     
     end
