17e7cca93d35fd058a6ad652062045976bc966f2
blureze <blureze@gmail.com>
2016-11-18 22:09:56 +0800

    update controllers/course.rb

--- controllers/course.rb
+++ controllers/course.rb
@@ -8,15 +8,20 @@ class ShareLearningAPI < Sinatra::Base
   # acquire all courses from database
   get "/#{API_VER}/courses/?" do
     udacity_results = FindAllCourses.call('Udacity')
-    udacity_courses = AllCoursesRepresenter.new(udacity_results.value).to_json  # output String object
-    udacity_courses = JSON.parse(udacity_courses) # parse String to JSON object
-
     coursera_results = FindAllCourses.call('Coursera')
-    coursera_courses = AllCoursesRepresenter.new(coursera_results.value).to_json  # output String object
-    coursera_courses = JSON.parse(coursera_courses) # parse String to JSON object
+    if udacity_results.success? && coursera_results.success?
+      udacity_courses = AllCoursesRepresenter.new(udacity_results.value).to_json  # output String object
+      udacity_courses = JSON.parse(udacity_courses) # parse String to JSON object
+  
+      coursera_courses = AllCoursesRepresenter.new(coursera_results.value).to_json  # output String object
+      coursera_courses = JSON.parse(coursera_courses) # parse String to JSON object
 
-    content_type 'application/json'
-    {udacity: udacity_courses['courses'], coursera: coursera_courses['courses'], youtube: 'inf'}.to_json
+      content_type 'application/json'
+      {udacity: udacity_courses['courses'], coursera: coursera_courses['courses'], youtube: 'inf'}.to_json
+    else
+      ErrorRepresenter.new(udacity_results.value).to_status_response if !udacity_results.success?
+      ErrorRepresenter.new(coursera_results.value).to_status_response if !coursera_results.success?
+    end
   end
 
   # find a course by its id
