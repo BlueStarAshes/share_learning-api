5cdfad174ec0ce3bd57a419c5001691bfb38d4fe
hsing-yu, Chen <andy19933@gmail.com>
2016-11-18 21:38:25 +0800

    Added require_relative info into representers/search_results.rb and representers/search_results_single_source.rb

--- representers/search_results.rb
+++ representers/search_results.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require_relative 'search_results_single_source'
 
 # Represents overall search results for JSON API output
 class SearchResultsRepresenter < Roar::Decorator
--- representers/search_results_single_source.rb
+++ representers/search_results_single_source.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require_relative 'search_results_course'
 
 # Represents course in search results for JSON API output
 class SearchResultsSingleSourceRepresenter < Roar::Decorator
