d51ec3f7db97e45c28ac7f86678acc37d2ee91e2
hsing-yu, Chen <andy19933@gmail.com>
2016-11-18 21:37:55 +0800

    Added require_relative info into representers/course_reviews_mapping_search_results.rb

--- representers/course_reviews_mapping_search_results.rb
+++ representers/course_reviews_mapping_search_results.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 require_relative 'review'
+require_relative 'course_reviews'
 
 # Represents the search results from 'Coursereview' table
 class CourseReviewsMappingSearchResultsRepresenter < Roar::Decorator
   include Roar::JSON
-  
+
   collection :course_reviews_mapping, extend: CourseReviewsRepresenter, class: Coursereview
 end
