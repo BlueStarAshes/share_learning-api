3d02890a556cd4c8b8c42f495ba8fe0c085f68c4
blureze <blureze@gmail.com>
2016-11-20 01:34:42 +0800

    finish refactoring course.rb(POST)

--- controllers/course.rb
+++ controllers/course.rb
@@ -38,43 +38,13 @@ class ShareLearningAPI < Sinatra::Base
 
   # store courses to database
   post "/#{API_VER}/courses/?" do
-    begin
-      # acquire all courses on Udacity
-      udacity_courses = Udacity::UdacityCourse.find.acquire_all_courses
-      udacity_courses.each do |course|
-        my_course = Course.create(
-          title: course[:title],
-          source: 'Udacity',
-          original_source_id: course[:id],
-          introduction: course[:intro], 
-          link: course[:link], 
-          photo: course[:image]
-        )
+    result = LoadCoursesFromAPI.call(params)
 
-      end
-
-      # acquire all courses on Coursera
-      coursera_courses = Coursera::CourseraCourses.find.courses
-      coursera_courses.each do |item|
-        item.each.with_index do |course, index| 
-          if index == 1
-            my_course = Course.create(
-              title: course[:course_name],
-              source: 'Coursera',
-              original_source_id: course[:course_id],
-              introduction: course[:description], 
-              link: course[:link], 
-              photo: course[:photo_url]
-            )  
-          end
-        end
-      end
-
+    if result.success?
       content_type 'text/plain'
-      body 'Add courses finish'
-    rescue
-      content_type 'text/plain'
-      halt 500, "Cannot create courses"
+      body result.value
+    else
+      ErrorRepresenter.new(result.value).to_status_response
     end
   end
 end
