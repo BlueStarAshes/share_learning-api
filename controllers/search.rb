10fa62643efc18b4380aa5a483e0648001c1fffb
hsing-yu, Chen <andy19933@gmail.com>
2016-11-19 20:17:14 +0800

    Used service object to refactor controllers/search.rb

--- controllers/search.rb
+++ controllers/search.rb
@@ -9,74 +9,9 @@ class ShareLearningAPI < Sinatra::Base
 
     if results.success?
       content_type 'application/json'
-      SearchResultsRepresenter.new(results).to_json
+      SearchResultsRepresenter.new(results.value).to_json
     else
       ErrorRepresenter.new(results.value).to_status_response
     end
   end
-=begin
-  get "/#{API_VER}/search/:search_keyword/?" do
-    keyword = params[:search_keyword]
-    keyword = keyword.tr('+', ' ') if keyword.include? '+'
-    begin
-      results_coursera =
-        Coursera::CourseraCourses.find.search_courses(:all, keyword)
-
-      results_udacity =
-        Udacity::UdacityCourse.find.acquire_courses_by_keywords(keyword)
-
-      results_youtube =
-        YouTube::YouTubePlaylist.find(keyword: keyword).results
-
-      refined_results_coursera = SearchResultsSingleSource.new(
-        results_coursera.count,
-        results_coursera.map do |course|
-          search_results_course = SearchResultsCourse.new(
-            course[:course_name],
-            course[:description],
-            course[:link],
-            course[:photo_url]
-          )
-          search_results_course
-        end
-      )
-
-      refined_results_udacity = SearchResultsSingleSource.new(
-        results_udacity.count,
-        results_udacity.map do |course|
-          search_results_course = SearchResultsCourse.new(
-            course[:title],
-            course[:intro],
-            course[:link],
-            course[:image]
-          )
-          search_results_course
-        end
-      )
-
-      refined_results_youtube = SearchResultsSingleSource.new(
-        results_youtube.count,
-        results_youtube.map do |course|
-          search_results_course = SearchResultsCourse.new(
-            course['title'],
-            course['description'],
-            course['url'],
-            course['image']
-          )
-          search_results_course
-        end
-      )
-
-      search_results = SearchResults.new(
-        refined_results_coursera,
-        refined_results_udacity,
-        refined_results_youtube
-      )
-
-      content_type 'application/json'
-      SearchResultsRepresenter.new(search_results).to_json
-
-    end
-  end
-=end
 end
--- services/search_courses.rb
+++ services/search_courses.rb
@@ -10,26 +10,109 @@ class SearchCourses
     keyword = params[:search_keyword]
     keyword = keyword.tr('+', ' ') if keyword.include? '+'
     if keyword
-      # Right(keyword: keyword)
-      Right(SearchResults.new)
+      results = SearchResults.new
+      Right(keyword: keyword, results: results)
     else
       Left(Error.new(:no_keyword, 'Keyword not given'))
     end
   }
 
   register :search_coursera, lambda { |input|
+    begin
+      results_coursera =
+        Coursera::CourseraCourses.find.search_courses(:all, input[:keyword])
 
+      refined_results_coursera = SearchResultsSingleSource.new(
+        results_coursera.count,
+        results_coursera.map do |course|
+          search_results_course = SearchResultsCourse.new(
+            course[:course_name],
+            course[:description],
+            course[:link],
+            course[:photo_url]
+          )
+          search_results_course
+        end
+      )
 
-    results = SearchResults.new
-    # input[:results].coursera = refined_results_coursera
-    # Right(input)
-    Right(results)
+      input[:results].coursera = refined_results_coursera
+      Right(input)
+    rescue
+      Left(
+        Error.new(
+          :search_exception_coursera,
+          'Exception during search in Coursera'
+        )
+      )
+    end
+  }
+
+  register :search_udacity, lambda { |input|
+    begin
+      results_udacity =
+        Udacity::UdacityCourse.find.acquire_courses_by_keywords(input[:keyword])
+
+      refined_results_udacity = SearchResultsSingleSource.new(
+        results_udacity.count,
+        results_udacity.map do |course|
+          search_results_course = SearchResultsCourse.new(
+            course[:title],
+            course[:intro],
+            course[:link],
+            course[:image]
+          )
+          search_results_course
+        end
+      )
+
+      input[:results].udacity = refined_results_udacity
+      Right(input)
+    rescue
+      Left(
+        Error.new(
+          :search_exception_udacity,
+          'Exception during search in Udacity'
+        )
+      )
+    end
+  }
+
+  register :search_youtube, lambda { |input|
+    begin
+      results_youtube =
+        YouTube::YouTubePlaylist.find(keyword: input[:keyword]).results
+
+      refined_results_youtube = SearchResultsSingleSource.new(
+        results_youtube.count,
+        results_youtube.map do |course|
+          search_results_course = SearchResultsCourse.new(
+            course['title'],
+            course['description'],
+            course['url'],
+            course['image']
+          )
+          search_results_course
+        end
+      )
+
+      input[:results].youtube = refined_results_youtube
+      Right(input[:results])
+    rescue
+      Left(
+        Error.new(
+          :search_exception_youtube,
+          'Exception during search in YouTube'
+        )
+      )
+    end
   }
 
   def self.call(params)
     Dry.Transaction(container: self) do
       step :parse_keyword
-      # step :search_coursera
+      step :search_coursera
+      step :search_udacity
+      step :search_youtube
     end.call(params)
   end
 end
