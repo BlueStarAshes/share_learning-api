d67ed2913574f2b6fc524ef20427546d63a47ed5
blureze <blureze@gmail.com>
2016-11-20 02:08:52 +0800

    finish refactoring review.rb(POST)

--- controllers/review.rb
+++ controllers/review.rb
@@ -32,34 +32,14 @@ class ShareLearningAPI < Sinatra::Base
   end
 
   post "/#{API_VER}/reviews/:course_id/?" do
-    begin
-      course_id = params[:course_id]
-      course = Course.find(id: course_id)
-      halt 400, "Course (id: #{course_id}) is not stored" unless course
+    input = {course_id: params[:course_id], request: request.body.read}
+    result = PostReview.call(input)
 
-      body_params = JSON.parse request.body.read
-      review_content = body_params['content']  
-      halt 400, "The review has no content to be stored" unless review_content
-
-      current_time = DateTime.now.strftime("%F %T")
-      
-
-      Review.create(
-        content: review_content,
-        created_time: current_time  # the time when the review is created
-      )
-
-      Coursereview.create(
-        course_id: course_id,
-        review_id: Review.last.id
-      )
-
+    if result.success?
       content_type 'text/plain'
-      body 'Successfully create a new review'
-
-    rescue
-      content_type 'text/plain'
-      halt 500, "Failed to create review"
+      body result.value
+    else
+      ErrorRepresenter.new(result.value).to_status_response
     end
   end
 end
---
+++ services/post_review.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+# Loads data from Facebook group to database
+class PostReview
+  extend Dry::Monads::Either::Mixin
+  extend Dry::Container::Mixin
+
+  register :validate_course, lambda { |params|
+    course_id = params[:course_id]
+    puts course_id
+    course = Course.find(id: course_id)    
+    
+    if course
+      Right(params)
+    else
+      Left(Error.new(:not_found, 'Course not found'))
+    end
+  }
+
+  register :validate_content, lambda { |params|
+    body_params = JSON.parse params[:request]
+    puts body_params
+    review_content = body_params['content']
+    
+    if review_content
+      Right({course_id: params[:course_id], review_content: review_content})
+    else
+      Left(Error.new(:not_found, 'The review has no content'))
+    end
+  }
+
+  register :create_review, lambda { |params|
+    begin
+      current_time = DateTime.now.strftime("%F %T")
+
+      review = Review.create(
+        content: params[:review_content],
+        created_time: current_time  # the time when the review is created
+      )
+
+      Right({review: review, course_id: params[:course_id]})
+    rescue
+      Left(Error.new(:bad_request, 'Failed to create review'))
+    end
+  }
+
+  register :create_review_course_mapping, lambda { |params|
+    begin
+      Coursereview.create(
+        course_id: params[:course_id],
+        review_id: params[:review].id
+      )
+
+      Right('Successfully create a new review')
+    rescue
+      Left(Error.new(:bad_request, 'Failed to create review'))
+    end
+  }
+
+  def self.call(params)
+    Dry.Transaction(container: self) do
+      step :validate_course
+      step :validate_content
+      step :create_review
+      step :create_review_course_mapping
+    end.call(params)
+  end
+end
