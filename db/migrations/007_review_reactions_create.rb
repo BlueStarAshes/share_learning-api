4f67e0667f662d49301479b0e7ebc498b87729c4
hsing-yu, Chen <andy19933@gmail.com>
2016-11-29 12:12:51 +0800

    Renamed db/migrations/007_reactions_on_reviews_create.rb -> db/migrations/007_review_reactions_create.rb

--- db/migrations/007_review_reactions_create.rb
+++ db/migrations/007_review_reactions_create.rb
@@ -5,7 +5,6 @@ Sequel.migration do
   change do
     create_table(:review_reactions) do
       primary_key :id
-      # primary_key [:review_id, :reaction_id], name: :items_pk
       foreign_key :review_id
       foreign_key :reaction_id
       Date :time
