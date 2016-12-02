b7f56f455226f8731d283f2c875c9caab00195e7
hsing-yu, Chen <andy19933@gmail.com>
2016-12-02 18:49:29 +0800

    Removed code that retrieves all courses from our own database in controllers/course.rb

--- controllers/course.rb
+++ controllers/course.rb
@@ -5,25 +5,6 @@ require 'json'
 class ShareLearningAPI < Sinatra::Base
   extend Econfig::Shortcut
 
-  # acquire all courses from database
-  get "/#{API_VER}/courses/?" do
-    udacity_results = FindAllCourses.call('Udacity')
-    coursera_results = FindAllCourses.call('Coursera')
-    if udacity_results.success? && coursera_results.success?
-      udacity_courses = AllCoursesRepresenter.new(udacity_results.value).to_json  # output String object
-      udacity_courses = JSON.parse(udacity_courses) # parse String to JSON object
-  
-      coursera_courses = AllCoursesRepresenter.new(coursera_results.value).to_json  # output String object
-      coursera_courses = JSON.parse(coursera_courses) # parse String to JSON object
-
-      content_type 'application/json'
-      {udacity: udacity_courses['courses'], coursera: coursera_courses['courses'], youtube: 'inf'}.to_json
-    else
-      ErrorRepresenter.new(udacity_results.value).to_status_response if !udacity_results.success?
-      ErrorRepresenter.new(coursera_results.value).to_status_response if !coursera_results.success?
-    end
-  end
-
   # find a course by its id
   get "/#{API_VER}/courses/:id/?" do
     result = FindCourse.call(params)
