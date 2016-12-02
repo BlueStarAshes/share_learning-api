4d4448f24016d6292de012eba27c744eba3c5aa8
hsing-yu, Chen <andy19933@gmail.com>
2016-12-02 17:04:19 +0800

    Deleted db/migrations/008_reactions_on_advanced_infos_for_courses_create.rb and models/reaction_on_advanced_info_for_course.rb, and modified the rest migration files' ordinal numbers

--- models/reaction.rb
+++ models/reaction.rb
@@ -3,6 +3,5 @@
 # Represents a Course's stored information
 class Reaction < Sequel::Model
   one_to_one :reviewReaction
-  one_to_one :reactionOnAdvancedInfoForCourse
   one_to_one :reactionOnPrerequisiteForCourse
 end
