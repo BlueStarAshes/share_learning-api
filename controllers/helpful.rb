d557fbf9988dd0b1de9186994db750fe04220919
blureze <blureze@gmail.com>
2016-12-06 17:12:35 +0800

    finish refactoring add helpful

--- controllers/helpful.rb
+++ controllers/helpful.rb
@@ -7,23 +7,7 @@ class ShareLearningAPI < Sinatra::Base
 
   # find helpful rating of a course
   get "/#{API_VER}/course/helpful/:course_id/?" do
-    result = FindCourse.call(params)
-
-    if result.success?
-      content_type 'application/json'
-      CourseRepresenter.new(result.value).to_json
-    else
-      ErrorRepresenter.new(result.value).to_status_response
-    end
-  end
-
-  # add helpful rating to course
-  post "/#{API_VER}/helpful/:course_id/?" do
     course_id = params[:course_id]
-    body_params = JSON.parse request.body.read
-    my_rating = body_params['rating']
-
-    print my_rating
 
     begin
       course = Course.find(id: course_id)
@@ -31,22 +15,43 @@ class ShareLearningAPI < Sinatra::Base
       content_type 'text/plain'
       halt 404, "Cannot find course"      
     end
-      
-    # add helpful rating
-    helpful_rating = Helpful.create(rating: my_rating)
 
-    # create helpful rating and course mapping
-    helpful_rating_id = helpful_rating.id
-    current_time = DateTime.now.strftime("%F %T")
-
-    CourseHelpful.create(
-      course_id: course_id,
-      helpful_id: helpful_rating_id,
-      created_time: current_time
-    )
+    results = CourseHelpfulsMappingSearchResults.new(CourseHelpful.where(course_id: course_id).all)
+    course_helpful_mapping = CourseHelpfulsMappingSearchResultsRepresenter.new(results).to_json
+    course_helpful_mapping = JSON.parse(course_helpful_mapping)
+    
+    course_helpfuls = course_helpful_mapping['course_helpfuls_mapping'].map do |helpful|
+      HelpfulsSearchResults.new(Helpful.find(id: helpful['helpful_id']))
+    end
+    #print course_helpfuls
+    
+    output = []
+    course_helpfuls.each do |course_helpful|
+      print course_helpful
+      helpful = HelpfulsSearchResultsRepresenter.new(course_helpful).to_json
+      #helpful = JSON.parse(helpful)
+      #print helpful
+      #output.push(helpful['rating'][0])
+    end
 
     content_type 'application/json'
-    {course_id: course_id, helpful_id: helpful_rating_id, created_time: current_time}.to_json
-
+    output.to_json
+    
   end
+
+  # add helpful rating to course
+  post "/#{API_VER}/helpful/:course_id/?" do
+    course_id = params[:course_id]
+    body_params = JSON.parse request.body.read
+    my_rating = body_params['rating']  
+     
+    input = {course_id: course_id, rating: my_rating}
+    result = AddHelpful.call(input)
+
+    if result.success?
+      content_type 'text/plain'
+      body result.value
+    else
+      ErrorRepresenter.new(result.value).to_status_response
+    end
 end
